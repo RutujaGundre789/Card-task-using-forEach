ef4160f19621ca317e3598a2d7598a9e4c3e9226
RutujaGundre789 <gundrerutuja789@gmail.com>
2025-12-14 14:37:14 +0530

    card task2 using forEach completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,29 @@
 const cl = console.log;
-const postsContainer = document.getElementById("postsContainer");
+const postContainer = document.getElementById("postContainer");
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 let result = ``;
-postsArr.forEach(function(post){
-	result +=`
-	           <div class="col-md-4 mb-4">
-				    <div class="card postcard h-100">
-					    <div class="card-header">
-						   <h3 class="m-0" data-toggle="tooltip" data-placement="top" title="${post.title}">
-						       ${post.title}
-						   </h3>
-						</div>
-						<div class="card-body">
-						    <p class="m-0">
-							    ${post.body}
-							</p>
-						</div>
-						<div class="card-footer text-right d-flex justify-content-between">
-						    <button class="btn btn-primary">Edit</button> 
-                            <button class="btn btn-danger">Delete</button>  							
-						</div>
-					</div>
-				</div>
-	         `
+postsArr.forEach(function(obj){
+	result += `
+	            <div class="col-md-4 mb-4">
+			      <div class="card postcard h-100">
+				      <div class="card-header">
+					      <h3 class="mb-0" data-toggle="tooltip" data-placement="top" title="${obj.title}">
+						  ${obj.title}
+						  </h3>
+					  </div>
+					  <div class="card-body">
+					     <p class="m-0">
+						    ${obj.body}
+						 </p>
+					  </div>
+					  <div class="card-footer text-right d-flex justify-content-between">
+					       <button class="btn btn-primary">Edit</button>
+						   <button class="btn btn-danger">Delete</button>
+					  </div>
+				  </div>
+			   </div>
+	          `
 })
-
-
-postsContainer.innerHTML = result;
+postContainer.innerHTML = result;
